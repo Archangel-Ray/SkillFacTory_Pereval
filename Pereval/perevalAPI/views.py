477f1496716395310087d42a3afff0be36b5f01f
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-10-06 16:06:27 +0300

    представления для всех моделей
    честно говоря, не совсем понимаю, что я тут делаю.

--- Pereval/perevalAPI/views.py
+++ Pereval/perevalAPI/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from Pereval import settings
+from .models import (
+    Coordinates,
+    Level,
+    Images,
+    SpecificationOfPereval,
+)
+from .serializers import (
+    UserSerializer,
+    SpecificationOfPerevalSerializer,
+    CoordinatesSerializer,
+    LevelSerializer,
+    ImagesSerializer,
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = settings.AUTH_USER_MODEL.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordinatesViewSet(viewsets.ModelViewSet):
+    queryset = Coordinates.objects.all()
+    serializer_class = CoordinatesSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class SpecificationOfPerevalViewSet(viewsets.ModelViewSet):
+    queryset = SpecificationOfPereval.objects.all()
+    serializer_class = SpecificationOfPerevalSerializer
