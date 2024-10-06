0aa79e1d0f99d03578e2fbe7aea9a4276362df9a
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-10-06 11:21:00 +0300

    прописал модель уровня сложности подъёма
    долго думал как лучше сделать. СкиллФакТори рекомендуют сделать
    это в основной модели, но как по мне она и так перегружена.
    по этому сделал отдельной таблицей, просто привяжу в одном поле.
    надеюсь это не станет ошибкой.

--- Pereval/perevalAPI/models.py
+++ Pereval/perevalAPI/models.py
@@ -14,3 +14,26 @@ class Coordinates(models.Model):
     latitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
     longitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
     height = models.IntegerField(null=True)
+
+
+class Level(models.Model):
+    difficulty_1 = 'I'
+    difficulty_2 = 'II'
+    difficulty_3 = 'III'
+    difficulty_4 = 'IV'
+    difficulty_5 = 'V'
+    difficulty_6 = 'VI'
+
+    LEVEL_CHOICES = {
+        (difficulty_1, 'Категория I'),
+        (difficulty_2, 'Категория II'),
+        (difficulty_3, 'Категория III'),
+        (difficulty_4, 'Категория IV'),
+        (difficulty_5, 'Категория V'),
+        (difficulty_6, 'Категория VI'),
+    }
+
+    spring = models.CharField(max_length=3, choices=LEVEL_CHOICES, default=difficulty_1, verbose_name='Весна')
+    summer = models.CharField(max_length=3, choices=LEVEL_CHOICES, default=difficulty_1, verbose_name='Лето')
+    autumn = models.CharField(max_length=3, choices=LEVEL_CHOICES, default=difficulty_1, verbose_name='Осень')
+    winter = models.CharField(max_length=3, choices=LEVEL_CHOICES, default=difficulty_1, verbose_name='Зима')
