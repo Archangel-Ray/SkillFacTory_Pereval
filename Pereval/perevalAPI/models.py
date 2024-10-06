43326b45290bb1823576b6722098828bdb789358
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-10-06 11:02:23 +0300

    модель координат
    теперь надо сделать вспомогательные таблицы, которые будут
    использоваться в основной.
    __________________________________________________________________
    создание базы прошло успешно. таблица пользователя создалась.
    нужные поля есть. по этому я слил ветки вместе. следующие
    несколько шагов ничего опасного не предполагают, по этому решил
    делать в основной ветке.

--- Pereval/perevalAPI/models.py
+++ Pereval/perevalAPI/models.py
@@ -8,3 +8,9 @@ class User(AbstractUser):
     name = models.CharField(max_length=50, verbose_name='Имя')
     otc = models.CharField(max_length=50, verbose_name='Отчество')
     phone = models.CharField(max_length=16, verbose_name='Телефон')
+
+
+class Coordinates(models.Model):
+    latitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    height = models.IntegerField(null=True)
