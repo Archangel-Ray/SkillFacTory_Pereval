896148bc569eabad6278c22882646268a24317c2
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-10-15 11:48:19 +0300

    добавил Сваггер в REST
    класс схемы.
    как я понял, собственно, здесь запускается создание схемы API

--- Pereval/Pereval/settings.py
+++ Pereval/Pereval/settings.py
@@ -128,5 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
