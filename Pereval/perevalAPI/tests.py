861e03a86a444247c6194a4000f15d969009899c
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-10-16 16:49:59 +0300

    тест сериализатора
    по тестированию вроде сделал всё что просили.
    осталось оформить описание и ... всё: финиш!
    а, ещё надо документацию Сваггера на хост установить.
    __________________________________________________________________
    ох и намаялся. и совсем не из за тестирования. главным образом из
    за часовых поясов. понаписал костылей, потому что понятия не имею
    как правильно обходить этот момент.

--- Pereval/perevalAPI/tests.py
+++ Pereval/perevalAPI/tests.py
@@ -1,3 +1,5 @@
+from datetime import timezone, timedelta
+
 from django.urls import reverse
 
 from rest_framework import status
@@ -89,3 +91,86 @@ class PerevalAPITestCase(ObjectsForTests):
         serializer_data = SpecificationOfPerevalSerializer(self.pereval1).data
         self.assertEqual(serializer_data, response.data)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+
+class PerevalSerializerTestCase(ObjectsForTests):
+    @staticmethod
+    def formatting(date):
+        moscow = timezone(timedelta(hours=3))
+        date = date.astimezone(moscow)
+        date = date.strftime('%Y-%m-%dT%H:%M:%S.%f%Z')  # .isoformat()
+        return date[:26] + date[29:]
+
+    def test_get_list(self):
+        serializer_data = SpecificationOfPerevalSerializer([self.pereval1, self.pereval2], many=True).data
+        expected_data = [
+            {
+                'beauty_title': 'гора.',
+                'title': 'Эльбрус',
+                'other_titles': 'Вершина',
+                'connect': '',
+                'add_time': self.formatting(self.pereval1.add_time),
+                'user': {
+                    'email': 'user1@mail.ru',
+                    'fam': 'Фамилия',
+                    'name': 'Имя',
+                    'otc': 'Отчество',
+                    'phone': '+0 000 000-00-00'
+                },
+                'coords': {
+                    'id': self.pereval1.coords.id,
+                    'latitude': '43.348788',
+                    'longitude': '42.445124',
+                    'height': 5642
+                },
+                'status': 'new',
+                'level': {
+                    'id': self.pereval1.level.id,
+                    'spring': 'V',
+                    'summer': 'V',
+                    'autumn': 'V',
+                    'winter': 'V'
+                },
+                'images': [
+                    {
+                        'data': 'https://turfirmarus.ru/assets/galleries/233/2.jpg',
+                        'title': 'Описание'
+                    },
+                ]
+            },
+            {
+                'beauty_title': 'гора.',
+                'title': 'Эльбрус',
+                'other_titles': 'Седловина',
+                'connect': '',
+                'add_time': self.formatting(self.pereval2.add_time),
+                'user': {
+                    'email': 'user1@mail.ru',
+                    'fam': 'Фамилия',
+                    'name': 'Имя',
+                    'otc': 'Отчество',
+                    'phone': '+0 000 000-00-00'
+                },
+                'coords': {
+                    'id': self.pereval2.coords.id,
+                    'latitude': '43.348788',
+                    'longitude': '42.445124',
+                    'height': 5642
+                },
+                'status': 'new',
+                'level': {
+                    'id': self.pereval2.level.id,
+                    'spring': 'IV',
+                    'summer': 'VI',
+                    'autumn': 'VI',
+                    'winter': 'IV'
+                },
+                'images': [
+                    {
+                        'data': 'https://cdn.culture.ru/images/beaf2c77-3b60-5c9f-97e6-e850fe5d8ec5',
+                        'title': 'Красивый вид на Эльбрус'
+                    },
+                ]
+            },
+        ]
+        self.assertEqual(serializer_data, expected_data)
