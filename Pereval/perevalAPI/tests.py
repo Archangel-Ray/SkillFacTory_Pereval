8c2ebb245842d9b83226eaf0ef92f6f583700b6c
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-10-16 13:10:49 +0300

    создание тестового объекта в базе
    принялся за самое непонятное для меня. пытался найти адекватные
    объяснения по тестированию REST-framework, но так что б я понял
    что здесь и куда найти не смог. нашёл подходящий пример. на его
    основе пытаюсь что-то изобразить.
    и так, запись в базе есть, сейчас попробую что-то с ней сделать.

--- Pereval/perevalAPI/tests.py
+++ Pereval/perevalAPI/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from perevalAPI.models import SpecificationOfPereval, User, Coordinates, Level, Images
+
+
+class PerevalAPITestCase(APITestCase):
+    def setUp(self):
+        self.pereval = SpecificationOfPereval.objects.create(
+            beauty_title='гора.',
+            title='Эльбрус',
+            other_titles='Вершина',
+            connect='',
+            user=User.objects.create(
+                email='user1@mail.ru',
+                fam='Фамилия',
+                name='Имя',
+                otc='Отчество',
+                phone='+0 000 000-00-00'
+            ),
+            coords=Coordinates.objects.create(
+                latitude='43.348788',
+                longitude='42.445124',
+                height='5642'
+            ),
+            level=Level.objects.create(
+                winter='V',
+                spring='V',
+                summer='V',
+                autumn='V'
+            ),
+        )
+
+        self.image_1 = Images.objects.create(
+            data='https://turfirmarus.ru/assets/galleries/233/2.jpg',
+            title='Описание',
+            pereval=self.pereval
+        )
